467a27ded1f9c97bb9cf34d065831fde4386a9bd
Annelies11 <mmahmudi1103@gmail.com>
2024-03-01 20:31:22 +0700

    sampek package

---
+++ helper/helper.go
@@ -0,0 +1,8 @@
+package helper
+
+var ver = "1.0.0"
+var App = "Golang"
+
+func SayHello(name string) string {
+	return "Hello, " + name
+}
---
+++ import.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"03_third/helper"
+	"fmt"
+)
+
+func main() {
+	res := helper.SayHello("Aris")
+
+	fmt.Println(res)
+	fmt.Println(helper.ver)
+}
